dcd6a8c0997390f7ab73b511ceb5f2b8ba2d957e
Steven Borrelli <steve@aster.is>
2016-06-29 13:07:06 +0200

    Add subnet module

---
+++ subnet/main.tf
@@ -0,0 +1,32 @@
+variable name { 
+   default = "terraform"
+   description = "Name of the virtual network"
+}
+
+variable address_prefixes {
+  default = [ "10.0.0.0/24" ] 
+  description = "List of CIDRs for the subnets."
+}
+
+variable resource_group_name {
+  default = ""
+  description = "Resource group name"
+}
+
+variable virtual_network_name {
+  default = ""
+  description = "Virtual network name"
+}
+
+resource "azurerm_subnet" "sb"  { 
+  count = "${length(var.address_prefixes)}" 
+  name  = "${var.name}-subnet-${count.index}"
+  resource_group_name = "${var.resource_group_name}"
+  virtual_network_name = "${var.virtual_network_name}"
+  address_prefix = "${element(var.address_prefixes,count.index)}"
+}
+
+output "ids" {
+  value = "${list(azurerm_subnet.sb.*.id)}"
+}
+
