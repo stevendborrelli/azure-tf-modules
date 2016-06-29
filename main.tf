cb263475883cd703933f38c812141b2cd8b11108
Steven Borrelli <steve@aster.is>
2016-06-29 13:12:06 +0200

    Add example main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable module_base { default = "github.com/stevendborrelli/azure-tf-modules/" }
+
+module "resource_group" {
+  source = "github.com/stevendborrelli/azure-tf-modules//resource_group"
+  name = "${var.name}"
+  location = "${var.location}"
+}
+
+module "virtual_network" {
+  source = "github.com/stevendborrelli/azure-tf-modules//virtual_network"
+  name = "${var.name}"
+  location = "${var.location}"
+  resource_group_name = "${module.resource_group.name}"
+  address_space = "${var.network_address_space}"
+}
+
+module "subnet" {
+  source = "github.com/stevendborrelli/azure-tf-modules//subnet"
+  name = "${var.name}"
+  resource_group_name = "${module.resource_group.name}"
+  virtual_network_name = "${module.virtual_network.name}"
+  address_prefixes = "${var.subnet_address_prefixes}"
+}
