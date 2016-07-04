dd1294326b9e31a0eb13a878eccc0e1bc0fd05b6
Steven Borrelli <steve@aster.is>
2016-07-04 15:22:36 -0500

    Add vm to example

--- main.tf
+++ main.tf
@@ -33,5 +33,24 @@ module "public_ips_control" {
   source = "github.com/stevendborrelli/azure-tf-modules//public_ip"
   location = "${var.location}"
   resource_group_name = "${module.resource_group.name}"
-  count = 3 
+  count = "${var.control_count}"
+}
+
+module "vms_control" {
+  source = "github.com/stevendborrelli/azure-tf-modules//virtual_machines"
+  name = "${var.name}-control"
+  count = "${var.control_count}"
+  vm_name = "control"
+  location = "${var.location}"
+  role = "role=control"
+  datacenter = "${var.datacenter}"
+  resource_group_name = "${module.resource_group.name}"
+  subnet_ids = "${module.subnet.ids}"
+  storage_account_name = "${module.storage_account.name}"
+  storage_primary_blob_endpoint = "${module.storage_account.primary_blob_endpoint}"
+  admin_username = "${var.admin_username}"
+  admin_password = "${var.admin_password}"
+#  public_ip_addresses = "${module.public_ips_control.ip_addresses}"
+  public_ip_address_ids = "${module.public_ips_control.ids}"
+#  network_security_group_id = "${module.network_security_group_default.id}"
 }
