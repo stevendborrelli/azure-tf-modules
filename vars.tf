0896338d8e8b288e4ac01c36da9a177fe0cfbc9d
Steven Borrelli <steve@aster.is>
2016-07-04 15:22:19 -0500

    Whitespace

--- vars.tf
+++ vars.tf
@@ -4,11 +4,11 @@ variable name { default = "sbterraform2" }
 variable network_address_space  { default = "10.0.0.0/16" }
 
 variable subnet_address_prefixes { 
-  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ] 
+  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
 }
 
 variable control_count { default = 3 }
-
+variable datacenter { default = "azure" }
 variable admin_username { default = "terraform" }
 variable admin_password { default = "TF-mantl1" }
 
