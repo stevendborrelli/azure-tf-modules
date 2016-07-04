4c1f84721eaa6cfc215135b824fa8799b0a4d1a4
Steven Borrelli <steve@aster.is>
2016-07-04 14:08:53 -0500

    Update vars.tf for control vms

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable location { default = "Central US" }
-variable name { default = "sbterraform" }
+variable name { default = "sbterraform2" }
 
 variable network_address_space  { default = "10.0.0.0/16" }
 
@@ -7,6 +7,9 @@ variable subnet_address_prefixes {
   default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ] 
 }
 
-variable public_ip_count_control {
-  default = 3 
-}
+variable control_count { default = 3 }
+
+variable admin_username { default = "terraform" }
+variable admin_password { default = "TF-mantl1" }
+
+
